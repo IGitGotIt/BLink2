eea605379b08a5e11e2f17180ca09e46329df3a3
IGitGotIt <jshah.jshah@gmail.com>
2020-05-06 18:08:50 -0700

    4

--- BLink2/BLink2/Classes/BlinkingLabel.swift
+++ BLink2/BLink2/Classes/BlinkingLabel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class BlinkingLabel : UILabel {
     public func startBlinking() {
-        let options : UIViewAnimationOptions = .repeat 
+        let options : UIView.AnimationOptions = .repeat 
         UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
             }, completion: nil)
